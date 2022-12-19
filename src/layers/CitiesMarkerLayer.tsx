5576f4f9d7f695ec012d7c7f52cabcafcba1d2e6
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-12-19 00:58:19 +0000

    Add cities layer.

---
+++ src/layers/CitiesMarkerLayer.tsx
@@ -0,0 +1,29 @@
+import { Marker, Popup } from "react-leaflet";
+
+interface Props {
+  data: GeoJSON.FeatureCollection;
+}
+
+export default function CitiesMarkerLayer({ data }: Props) {
+  return (
+    <>
+      {data.features.map((x: GeoJSON.Feature) => {
+        if (x.geometry.type !== "Point") {
+          return <></>;
+        }
+        const { coordinates } = x.geometry;
+
+        return (
+          <Marker
+            key={String(coordinates)}
+            position={[coordinates[1], coordinates[0]]}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        );
+      })}
+    </>
+  );
+}
--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,21 +1,16 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-
+import { MapContainer, TileLayer } from "react-leaflet";
+import { cities } from "../data/cities";
+import CitiesMarkerLayer from "../layers/CitiesMarkerLayer";
 import "./Map.css";
 
 export default function Map() {
-  const position: [number, number] = [51.505, -0.09];
-
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <CitiesMarkerLayer data={cities} />
     </MapContainer>
   );
 }

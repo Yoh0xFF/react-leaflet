ab20af12c748529f54b9acc9ecb64c8bb7cfd055
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-12-26 16:14:35 +0000

    Add esri world imagery base layer.

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -51,6 +51,13 @@ export default function Map() {
           />
         </LayersControl.BaseLayer>
 
+        <LayersControl.BaseLayer name='ESRI World Imagery Layer'>
+          <TileLayer
+            attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+            url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+          />
+        </LayersControl.BaseLayer>
+
         {cities && (
           <CitiesMarkerLayer
             data={cities}
